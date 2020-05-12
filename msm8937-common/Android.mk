4cc5703e1f0d2ddd40e0e9463889441cdc8af124
wh0dat <whodat@remainsilent.net>
2020-05-12 21:51:43 +0000

    msm8937-common: Add support for james
    Signed-off-by: wh0dat <whodat@remainsilent.net>
    Change-Id: I1a45d8abd4165a6ca26927ca1ce2bcccbafb9407

--- msm8937-common/Android.mk
+++ msm8937-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hannah,$(TARGET_DEVICE)),)
+ifneq ($(filter hannah james,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libsdm-disp-vndapis
